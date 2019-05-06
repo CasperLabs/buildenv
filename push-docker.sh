83a4b03cd6d8939c9c74891296fa677313cdf7f7
Tom Vasile <43349666+TomVasile@users.noreply.github.com>
2019-05-06 16:13:21 -0400

    adding variables

--- push-docker.sh
+++ push-docker.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
+
+N=casperlabs/buildenv
+C=${DRONE_COMMIT_SHA:-$(git rev-parse --short HEAD)}
+git fetch -t
+V=$(git describe --tags --always)
+
 builtin echo $DOCKER_PASSWORD | docker login -u $DOCKER_USERNAME --password-stdin
 
 set -x
